46ae98a2500679a6f03035f64d8eeed24aaae062
Valerytintaya <distol303@gmail.com>
2025-03-18 23:31:05 -0500

    Delete C++ directory

--- C++/Experiencia 5/Pedido.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pedido.h"
-
-void Pedido::agregarProducto(const Producto& producto, int cantidad) {
-    listaDeProductos.push_back({producto, cantidad});
-}
-
-std::vector<std::pair<Producto, int>> Pedido::getListaDeProductos() const {
-    return listaDeProductos;
-}
